675dc7b140c91a4e3dbfb257ad32603f4e395c82
Arindo-Mudiare <ArindoTmudiare@gmail.com>
2022-07-18 18:25:04 +0100

    completed middleware set up

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/register", async (req, res) => {
   try {
@@ -55,6 +56,30 @@ router.post("/login", async (req, res) => {
     res
       .status(500)
       .send({ message: "Error Authenticating User", success: false, error });
+  }
+});
+
+router.post("/get-user-info-by-id", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.body.userId });
+    if (!user) {
+      return res
+        .status(200)
+        .send({ message: "User does not exist", success: false });
+    } else {
+      res.status(200).send({
+        success: true,
+        data: {
+          name: user.name,
+          email: user.email,
+        },
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({ message: "Error getting user info", success: false, error });
   }
 });
 
