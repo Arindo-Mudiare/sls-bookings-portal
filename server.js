0c7a4cf1148cf2d4d08c94fa580224d76006482c
Arindo-Mudiare <ArindoTmudiare@gmail.com>
2022-07-18 01:34:38 +0100

    Completed API Registration and Login functionality

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const dbConfig = require("./config/db.config");
+app.use(express.json());
+const userRoute = require("./routes/userRoute");
 
+app.use("/api/user", userRoute);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Node Server Listening on ${port}`));
