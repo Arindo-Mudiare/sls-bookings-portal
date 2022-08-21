70254726f47b695dc2aa721116162c345d0d3dfc
Arindo-Mudiare <ArindoTmudiare@gmail.com>
2022-08-21 17:20:30 +0100

    enabled https redirect for production

--- server.js
+++ server.js
@@ -21,13 +21,13 @@ const path = require("path");
 // app.use(cors(corsOptions));
 
 // ssl redirect in heroku functionality
-// app.use(function (req, res, next) {
-//   if (req.header("x-forwarded-proto") === "http") {
-//     res.redirect(301, "https://" + req.hostname + req.url);
-//     return;
-//   }
-//   next();
-// });
+app.use(function (req, res, next) {
+  if (req.header("x-forwarded-proto") === "http") {
+    res.redirect(301, "https://" + req.hostname + req.url);
+    return;
+  }
+  next();
+});
 
 app.use("/api/user", userRoute);
 app.use("/api/password-reset", passwordResetRoutes);
